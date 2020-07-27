d22cb0bbcecfdcefcc1755a4640880f7c0478cc9
Dario <soliderdario@hotmail.com>
2020-07-27 13:28:44 -0300

    Backend -> Finished test

--- Logstore/src/Logstore.Sale/Controllers/V1/HistoryController.cs
+++ Logstore/src/Logstore.Sale/Controllers/V1/HistoryController.cs
@@ -26,7 +26,7 @@ namespace Logstore.Sale.Controllers.V1
         }
 
         [HttpGet("query/email")]
-        public async Task<IEnumerable<OrderHistoryView>> Query(string email)
+        public async Task<IEnumerable<OrderHistoryView>> QueryEmail(string email)
         {
             try
             {
@@ -53,6 +53,44 @@ namespace Logstore.Sale.Controllers.V1
                     }
                     orders.Where(src => src.OrderId == order.OrderId).FirstOrDefault().Pizzas.AddRange(items);
                 }                
+                return orders;
+            }
+            catch (Exception ex)
+            {
+                NotifyError(ex.Message);
+                return (IEnumerable<OrderHistoryView>)BadRequest(ex.Message);
+            }
+        }
+
+
+        [HttpGet("query/orderid")]
+        public async Task<IEnumerable<OrderHistoryView>> QueryOrderId(long orderId)
+        {
+            try
+            {
+                var sql = "SELECT O.Id as OrderId, c.Name as CustomerName, O.DateCreate, O.Total " +
+                         " FROM Customer C " +
+                         " INNER JOIN[Order] O on O.CustomerId = C.Id " +
+                         " WHERE O.Id = @orderId" +
+                         " ORDER BY O.DateCreate desc";
+                var orders = await _orderRepository.Query<OrderHistoryView>(sql, new { orderId });
+
+                foreach (var order in orders)
+                {
+                    sql = "SELECT Id as OrderitemId " +
+                          " FROM OrderItem " +
+                          " WHERE OrderId =@OrderId";
+                    var items = await _orderRepository.Query<OrderHistoryItemView>(sql, new { order.OrderId });
+                    foreach (var item in items)
+                    {
+                        sql = "SELECT F.Name as Flavor, OIF.Value FROM OrderItemFlavor OIF " +
+                              " INNER JOIN Flavor F on F.Id = OIF.FlavorId " +
+                              " WHERE OIF.OrderItemId = @OrderItemId";
+                        var flavors = await _orderRepository.Query<OrderHistoryItemFlavorView>(sql, new { item.OrderItemId });
+                        items.Where(src => src.OrderItemId == item.OrderItemId).FirstOrDefault().Flavors.AddRange(flavors);
+                    }
+                    orders.Where(src => src.OrderId == order.OrderId).FirstOrDefault().Pizzas.AddRange(items);
+                }
                 return orders;
             }
             catch (Exception ex)
